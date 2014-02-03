d7c6ba682434b8ba931eda9c411abd7e59938912
Antonas <anton.as@me.com>
2014-02-03 14:44:11 +0100

    Added isbn and title ivars with getters and setters

--- BookKeeper/src/com/example/bookkeeper/Book.java
+++ BookKeeper/src/com/example/bookkeeper/Book.java
@@ -1,5 +1,31 @@
 package com.example.bookkeeper;
 
-public class Book {
-
+public class Book 
+{
+	private String isbn;
+	private String title;
+	
+	
+	
+	public void setIsbn(String isbn)
+	{
+		this.isbn = isbn;
+	}
+	
+	public String getIsbn()
+	{
+		return this.isbn;
+	}
+	
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+	
+	public String getTitle()
+	{
+		return this.title;
+	}
+	
+	
 }
