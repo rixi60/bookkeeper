653b7bf1e5d5e8eb61c1b3b4dbba5409d431ef58
Antonas <anton.as@me.com>
2014-02-03 17:53:48 +0100

    Added the last ivars with getters and setters. Method for adding a author: addAuthor(String author)

--- BookKeeper/src/com/example/bookkeeper/Book.java
+++ BookKeeper/src/com/example/bookkeeper/Book.java
@@ -1,9 +1,18 @@
 package com.example.bookkeeper;
 
+import java.util.List;
+
 public class Book 
 {
 	private String isbn;
 	private String title;
+	private List<String> authors;
+	private String description;
+	private String language;
+	private int pageCount;
+	private int published;
+	private String thumbnailURL;
+	private String loaner;
 	private String comment;
 	
 	
@@ -27,7 +36,31 @@ public class Book
 	{
 		return this.title;
 	}
+	
 
+	/**
+	 * @return the authors
+	 */
+	public List<String> getAuthors() {
+		return authors;
+	}
+
+	/**
+	 * @param authors the authors to set
+	 */
+	public void setAuthors(List<String> authors) {
+		this.authors = authors;
+	}
+
+	/**
+	 * Adds an author to the book.
+	 * @param authors the authors to add
+	 */
+	public void addAuthor(String author)
+	{
+		this.authors.add(author);
+	}
+	
 	/**
 	 * @return the comment
 	 */
@@ -40,6 +73,90 @@ public class Book
 	 */
 	public void setComment(String comment) {
 		this.comment = comment;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the language
+	 */
+	public String getLanguage() {
+		return language;
+	}
+
+	/**
+	 * @param language the language to set
+	 */
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	/**
+	 * @return the pageCount
+	 */
+	public int getPageCount() {
+		return pageCount;
+	}
+
+	/**
+	 * @param pageCount the pageCount to set
+	 */
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+
+	/**
+	 * @return the published
+	 */
+	public int getPublished() {
+		return published;
+	}
+
+	/**
+	 * @param published the published to set
+	 */
+	public void setPublished(int published) {
+		this.published = published;
+	}
+
+	/**
+	 * @return the thumbnailURL
+	 */
+	public String getThumbnailURL() {
+		return thumbnailURL;
+	}
+
+	/**
+	 * @param thumbnailURL the thumbnailURL to set
+	 */
+	public void setThumbnailURL(String thumbnailURL) {
+		this.thumbnailURL = thumbnailURL;
+	}
+
+	/**
+	 * @return the loaner
+	 */
+	public String getLoaner() {
+		return loaner;
+	}
+
+	/**
+	 * @param loaner the loaner to set
+	 */
+	public void setLoaner(String loaner) {
+		this.loaner = loaner;
 	}
 	
 	
