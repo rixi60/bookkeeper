0469aafe2808818cf0f42dd2ee3ad367e9a45e15
Jeppe <Jeppe@Jeppe-PC>
2014-02-05 12:41:08 +0100

    Book info

---
+++ BookKeeper/src/edu/kea/pm/bookkeeper/activity/BookInfoActivity.java
@@ -0,0 +1,18 @@
+package edu.kea.pm.bookkeeper.activity;
+
+import edu.kea.pm.bookkeeper.R;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class BookInfoActivity extends FragmentActivity
+{
+
+	@Override
+	protected void onCreate(Bundle bundle)
+	{
+		super.onCreate(bundle);
+		setContentView(R.layout.activity_single_frame_container);
+	}
+	
+}
