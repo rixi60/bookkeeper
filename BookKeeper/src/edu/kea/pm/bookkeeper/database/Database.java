6c0a10ee32979980f3cf8a53ef45f3eeceff0114
Antonas <anton.as@me.com>
2014-02-05 13:39:41 +0100

    Added interface-method

--- BookKeeper/src/edu/kea/pm/bookkeeper/database/Database.java
+++ BookKeeper/src/edu/kea/pm/bookkeeper/database/Database.java
@@ -1,6 +1,20 @@
 package edu.kea.pm.bookkeeper.database;
 
+import java.util.List;
+
+import android.database.Cursor;
+
+import edu.kea.pm.bookkeeper.model.*;
+
 public interface Database
 {
-
+	public Book getBookWithId(String id);
+	public Cursor getAllBooks();
+	public List<Book> getBooksWithISBN(String isbn);
+	public List<Book> getBooksWithAuthor(String authorName);
+	public List<Book> getBooksWithTitle(String title);
+	public List<Book> getLoanedBooks();
+	public List<Book> getBooksLoanedByPerson(String personName);
+	public List<Book> getBooksWithPublishedYear(int year);		
+	
 }
